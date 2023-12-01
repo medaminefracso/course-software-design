f3be04e4558835d423ee7d9d38288defa564d068
medaminefracso <medaminefracso@gmail.com>
2023-12-01 15:59:18 +0100

    implement Builder pattern

---
+++ src/main/java/builderpatternmyhomework/App.java
@@ -0,0 +1,18 @@
+package builderpatternmyhomework;
+
+public class App {
+
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle.Builder()
+                .price(10300.76)
+                .type("car")
+                .make("Honda")
+                .horsePower(150)
+                .model("Accord")
+                .color("red")
+                .doors(4)
+                .build();
+
+        System.out.println(car);
+    }
+}
